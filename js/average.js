dd582316d79c703c3635bc2e0b1d7832a8c25bcb
calcify21 <jainshr21@outlook.com>
2024-08-08 15:37:32 +0000

    hello

--- js/average.js
+++ js/average.js
@@ -1,16 +1,17 @@
 function findAverage() {
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
-    let result = document.getElementById("result");
-    result.style.display = "block";
-    let sum = num1 + num2;
-    let avg = sum / 2;
+    // const num1 = parseFloat(document.getElementById("num1").value);
+    // const num2 = parseFloat(document.getElementById("num2").value);
+    // let result = document.getElementById("result");
+    // result.style.display = "block";
+    // let sum = num1 + num2;
+    // let avg = sum / 2;
 
-    if (isNaN(avg)) {
-        result.textContent = "Please enter valid numbers.";
-        result.classList.replace("alert-success", "alert-danger")
-    } else {
-        result.textContent = `The average of ${num1} and ${num2} is ${avg}.`;
-        result.classList.replace("alert-danger", "alert-success")
-    }
+    // if (isNaN(avg)) {
+    //     result.textContent = "Please enter valid numbers.";
+    //     result.classList.replace("alert-success", "alert-danger")
+    // } else {
+    //     result.textContent = `The average of ${num1} and ${num2} is ${avg}.`;
+    //     result.classList.replace("alert-danger", "alert-success")
+    // }
+
 }
