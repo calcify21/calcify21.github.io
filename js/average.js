d32a603e28dc979cc415f090ff32c67e7a0e3b5c
calcify21 <jainshr21@outlook.com>
2024-08-09 13:53:44 +0000

    add field option to average.html and js/average.js

--- js/average.js
+++ js/average.js
@@ -1,17 +1,36 @@
+function addField() {
+    const newInputCount = document.getElementsByClassName("num-input").length + 1
+    const newInput = document.createElement("div");
+    newInput.className = "form-floating mb-3";
+    const newInputLabel = document.createElement("label");
+    newInputLabel.htmlFor = `num${newInputCount}`;
+    newInputLabel.textContent = `Number ${newInputCount}`;
+    const newInputField = document.createElement("input");
+    newInputField.type = "number";
+    newInputField.className = "form-control num-input";
+    newInputField.placeholder = `Enter number ${newInputCount}`;
+    newInputField.id = `num${newInputCount}`;
+    newInput.appendChild(newInputField);
+    newInput.appendChild(newInputLabel);
+    document.getElementById("input-container").appendChild(newInput);
+}
+
 function findAverage() {
-    // const num1 = parseFloat(document.getElementById("num1").value);
-    // const num2 = parseFloat(document.getElementById("num2").value);
-    // let result = document.getElementById("result");
-    // result.style.display = "block";
-    // let sum = num1 + num2;
-    // let avg = sum / 2;
-
-    // if (isNaN(avg)) {
-    //     result.textContent = "Please enter valid numbers.";
-    //     result.classList.replace("alert-success", "alert-danger")
-    // } else {
-    //     result.textContent = `The average of ${num1} and ${num2} is ${avg}.`;
-    //     result.classList.replace("alert-danger", "alert-success")
-    // }
-
+    const numInputs = document.getElementsByClassName("num-input");
+    let sum = 0;
+    let count = 0;
+    let result = document.getElementById("result");
+    result.style.display = "block";
+    for (let i = 0; i < numInputs.length; i++) {
+        const num = parseFloat(numInputs[i].value);
+        if (!isNaN(num)) {
+            sum += num;
+            count++;
+            result.classList.replace("alert-danger", "alert-success")
+            result.textContent = `The average is ${sum / count}`;
+        } else {
+            result.textContent = "Please enter valid numbers.";
+            result.classList.replace("alert-success", "alert-danger")
+        }
+    }
 }
