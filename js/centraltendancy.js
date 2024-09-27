85d1cfc2b86e33a37c0546a74607304dd64964d1
Shaurya Jain <jainshr21@outlook.com>
2024-09-27 10:46:47 +0000

    addons

--- js/centraltendancy.js
+++ js/centraltendancy.js
@@ -1,69 +1,59 @@
 function addField() {
     const newInputCount = document.getElementsByClassName("num-input").length + 1;
-    // * Row
-    const newInput = document.createElement("div");
-    newInput.className = "row";
-    // * Col 1
-    const newCol1 = document.createElement("div");
-    newCol1.className = "col";
-    // * Input Field and Label Div
-    const newDiv = document.createElement("div");
-    newDiv.className = "form-floating mb-3";
-    // * Input Field
-    const newInputField = document.createElement("input");
-    newInputField.type = "number";
-    newInputField.className = "form-control num-input";
-    newInputField.placeholder = `Enter number ${newInputCount}`;
-    newInputField.id = `num${newInputCount}`;
-    // * Input Label
-    const newInputLabel = document.createElement("label");
-    newInputLabel.htmlFor = `num${newInputCount}`;
-    newInputLabel.textContent = `Number ${newInputCount}`;
-    // * Col 2
-    const newCol2 = document.createElement("div");
-    newCol2.className = "col-1";
-    // * Remove Button
-    const newRemoveBtn = document.createElement("button");
-    newRemoveBtn.className = "fa-solid fa-xmark-circle btn text-danger";
-    newRemoveBtn.style.fontSize = "30px";
-    // * Append all new elements
-    document.getElementById("input-container").appendChild(newInput);
-    newInput.appendChild(newCol1);
-    newCol1.appendChild(newDiv);
-    newDiv.appendChild(newInputField);
-    newDiv.appendChild(newInputLabel);
-    newInput.appendChild(newCol2);
-    newCol2.appendChild(newRemoveBtn);
+    const ordinalNum = utils().getOrdinalSuffixOf(newInputCount);
+    // // * Row
+    // const newInput = document.createElement("div");
+    // newInput.className = "row";
+    // // * Col 1
+    // const newCol1 = document.createElement("div");
+    // newCol1.className = "col";
+    // // * Input Field and Label Div
+    // const newDiv = document.createElement("div");
+    // newDiv.className = "form-floating mb-3";
+    // // * Input Field
+    // const newInputField = document.createElement("input");
+    // newInputField.type = "number";
+    // newInputField.className = "form-control num-input";
+    // newInputField.placeholder = `Enter number ${newInputCount}`;
+    // newInputField.id = `num${newInputCount}`;
+    // // * Input Label
+    // const newInputLabel = document.createElement("label");
+    // newInputLabel.htmlFor = `num${newInputCount}`;
+    // newInputLabel.textContent = `Enter ${ordinalNum} Number`;
+    // // * Col 2
+    // const newCol2 = document.createElement("div");
+    // newCol2.className = "col-1";
+    // // * Remove Button
+    // const newRemoveBtn = document.createElement("button");
+    // newRemoveBtn.className = "fa-solid fa-xmark-circle btn text-danger";
+    // newRemoveBtn.style.fontSize = "30px";
+    // // * Append all new elements
+    // document.getElementById("input-container").appendChild(newInput);
+    // newInput.appendChild(newCol1);
+    // newCol1.appendChild(newDiv);
+    // newDiv.appendChild(newInputField);
+    // newDiv.appendChild(newInputLabel);
+    // newInput.appendChild(newCol2);
+    // newCol2.appendChild(newRemoveBtn);
+
+    let newInputCtrlHtml = `<div class="form-floating mb-3 input${newInputCount}">
+    <div class="input-group">
+        <input type="number" class="form-control num-input" placeholder="Enter ${ordinalNum} number"
+            id="num${newInputCount}">
+        <span class="input-group-text" id="basic-addon2">
+        <button class="fa-solid fa-xmark-circle btn text-danger"
+                style="font-size: 30px;" id=removeBtn${newInputCount}></button>
+                </span>
+    </div>
+</div>`;
+    document.getElementById("input-container").insertAdjacentHTML("beforeend", newInputCtrlHtml);
+
     // * Event Listener for Remove Button
-    newRemoveBtn.addEventListener("click", function (event) {
+    document.getElementById(`removeBtn${newInputCount}`).addEventListener("click", function (event) {
         event.preventDefault();
-        newInput.remove();
+        document.querySelector(`.input${newInputCount}`).remove();
     });
-    // }
-
-    // function removeField(newInput) {
-    //     newInput.remove();
 }
-
-// function find() {
-//     const numInputs = document.getElementsByClassName("num-input");
-//     let sum = 0;
-//     let count = 0;
-//     let result = document.getElementById("result");
-//     result.style.display = "block";
-//     for (let i = 0; i < numInputs.length; i++) {
-//         const num = parseFloat(numInputs[i].value);
-//         if (!isNaN(num)) {
-//             sum += num;
-//             count++;
-//             result.classList.replace("alert-danger", "alert-success");
-//             result.textContent = `The average is ${sum / count}`;
-//         } else {
-//             result.textContent = "Please enter valid numbers.";
-//             result.classList.replace("alert-success", "alert-danger");
-//         }
-//     }
-// }
 
 function check() {
     const numInputs = document.getElementsByClassName("num-input");
@@ -163,14 +153,7 @@ function resetFields() {
     resultsTable.style.display = "none";
     const inputContainer = document.getElementById("input-container");
     result.style.display = "none";
-    inputContainer.innerHTML = `<div class="form-floating mb-3">
-                        <input type="number" class="form-control num-input" placeholder="Enter number 1" id="num1">
-                        <label for="num1">Number 1</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="number" class="form-control num-input" placeholder="Enter number 2" id="num2">
-                        <label for="num2">Number 2</label>
-                    </div>`;
+    inputContainer.innerHTML = "";
 }
 
 const toastTrigger = document.getElementById("reset1");
