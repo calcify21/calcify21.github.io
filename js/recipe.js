deef043c58e14a3d8116c350b960679cd78752a4
Shaurya Jain <jainshr21@outlook.com>
2024-10-06 13:28:51 +0000

    fixed recipe.html modal issue

--- js/recipe.js
+++ js/recipe.js
@@ -1,12 +1,22 @@
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
+let recipes = [];
 
 function fetchRecipes(query) {
     const fetchData = fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
         .then(response => response.json())
         .then(data => {
-            showRecipes(data.meals);
-            console.log(data.meals);
+            recipes = data.meals;
+            if (recipes === null) {
+                document.querySelector(".heading").textContent = "No recipe found for this query.";
+                document.querySelector(".heading").classList.add("text-danger");
+                document.querySelector("#recipe-container").innerHTML = "";
+            } else {
+                document.querySelector(".heading").innerHTML = `Showing ${recipes.length} recipes for "${query}": `;
+                document.querySelector(".heading").classList.remove("text-danger");
+                showRecipes();
+            }
+            // console.log(data.meals);
         })
         .catch(error => console.log(error));
 
@@ -18,11 +28,10 @@ searchBtn.addEventListener("click", (e) => {
     fetchRecipes(searchTerm)
 })
 
-function showRecipes(recipes) {
+function showRecipes() {
     const recipeContainer = document.getElementById("recipe-container");
-    const modalContainer = document.getElementById("modals");
     recipeContainer.innerHTML = "";
-    recipes.forEach(recipe => {
+    recipes.forEach((recipe, index) => {
         const recipeTitle = recipe.strMeal;
         const recipeImage = recipe.strMealThumb;
         const recipeArea = recipe.strArea;
@@ -33,39 +42,16 @@ function showRecipes(recipes) {
                 <div class="card-body">
                     <h5 class="card-title">${recipeTitle}</h5>
                     <p class="card-text"><span class="fw-bold">${recipeArea}</span> Dish</p>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#recipeModal" id=${recipes.indexOf(recipe)} onclick="showModal(${recipes.indexOf(recipe)}, ${JSON.stringify(recipes)})">View Recipe</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#recipeModal" onclick="showModal(${index})">View Recipe</button>
                 </div>
             </div>
         </div>
         `
-        // const recipeItemModalCtrl = `
-        // <div class="modal fade" id="recipeModal" tabindex="-1" aria-labelledby="label" aria-hidden="true">
-        //     <div class="modal-dialog modal-dialog-scrollable">
-        //         <div class="modal-content">
-        //             <div class="modal-header">
-        //                 <h1 class="modal-title fs-5" id="label">Modal title</h1>
-        //                 <button type="button" class="btn-close" data-bs-dismiss="modal"
-        //                             aria-label="Close"></button>
-        //             </div>
-        //             <div class="modal-body">
-        //                 <h4 class="fw-bold">Ingredients:</h4>
-        //                 <p>${getIngredients(recipe)}</p>
-        //                 <h4 class="fw-bold">Instructions:</h4>
-        //                 <p>${recipe.strInstructions}</p>
-        //             </div >
-        //             <div class="modal-footer">
-        //                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        //                 <a class="btn btn-primary" href="" role="button">Open recipe in Youtube</a>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        //     `
-        // const formattedText = recipeItemModalCtrl.replace(/\r\n/g, '<br>');
         recipeContainer.insertAdjacentHTML("beforeend", recipeItemHtmlCtrl);
-        // modalContainer.insertAdjacentHTML("beforeend", formattedText);
     });
 }
+
+
 
 const getIngredients = (recipe) => {
     let ingredientList = "";
@@ -81,43 +67,16 @@ const getIngredients = (recipe) => {
     return ingredientList;
 }
 
-function showModal(id, recipes) {
-    // const modal = document.getElementById("recipeModal");
+function showModal(id) {
+    let ingredients = document.querySelector(".ingredients")
+    let instructions = document.querySelector(".instructions")
+    let recipeLink = document.querySelector(".recipe-link")
+    let modalTitle = document.querySelector(".modal-title");
     const recipe = recipes[id];
-    // const modalTitle = modal.querySelector(".modal-title");
-    // const modalBody = modal.querySelector(".modal-body");
-    // const modalFooter = modal.querySelector(".modal-footer a");
-    // modalTitle.textContent = recipe.strMeal;
-    // modalBody.innerHTML = `
-    //     <h4 class="fw-bold">Ingredients:</h4>
-    //     <p>${getIngredients(recipe)}</p>
-    //     <h4 class="fw-bold">Instructions:</h4>
-    //     <p>${recipe.strInstructions}</p>
-    //     `;
-    // modalFooter.href = `https://www.youtube.com/results?search_query=${recipe.strMeal}+recipe`;
-    const recipeItemModalCtrl = `
-        <div class="modal fade" id="recipeModal" tabindex="-1" aria-labelledby="label" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="label">Modal title</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <h4 class="fw-bold">Ingredients:</h4>
-                        <p>${getIngredients(recipe)}</p>
-                        <h4 class="fw-bold">Instructions:</h4>
-                        <p>${recipe.strInstructions}</p>
-                    </div >
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <a class="btn btn-primary" href="" role="button">Open recipe in Youtube</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-            `
-    const formattedText = recipeItemModalCtrl.replace(/\r\n/g, '<br>');
-    document.getElementById("modals").insertAdjacentHTML("beforeend", formattedText);
+    const recipeInstructions = recipe.strInstructions.replace(/\r\n/g, '<br>');
+    const recipeIngredients = getIngredients(recipe);
+    recipeLink.href = recipe.strYoutube;
+    instructions.innerHTML = recipeInstructions;
+    ingredients.innerHTML = recipeIngredients;
+    modalTitle.innerHTML = recipe.strMeal;
 }
