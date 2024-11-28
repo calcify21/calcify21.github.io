4400b5a1fb2971937e3bb20f9b4b60f0dbd886f1
Shaurya Jain <175240047+calcify21@users.noreply.github.com>
2024-11-28 15:58:35 +0000

    navbar collapse home tab issue fixed

--- js/fav.js
+++ js/fav.js
@@ -1,8 +1,10 @@
 function loadFavorites() {
     let favorites = JSON.parse(localStorage.getItem('favorites'));
     let container = document.getElementById('favorites-container');
+    let removeAllBtn = document.getElementById('removeFavoritesBtn');
     if (favorites == undefined || favorites.length == 0) {
         container.innerHTML = '<div class="text-center alert alert-warning" role="alert">You do not have any favorite calculators yet. Click on the star icon on the <a href="calcs.html" class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">calculators page</a> to add a calculator to your list.</div>';
+        removeAllBtn.style.display = 'none';
     } else {
         favorites.forEach(id => {
             let calculatorHtml = generateCalculatorHtml(id);
