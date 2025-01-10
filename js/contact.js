9beb10265791684e47d222f08daafac77c2aac83
Shaurya Jain <175240047+calcify21@users.noreply.github.com>
2025-01-10 04:36:01 +0000

    updates to contact form

--- js/contact.js
+++ js/contact.js
@@ -1,25 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var iframe = document.getElementById('gForm');
-    var spinner = document.getElementById('spinner');
-    var tickContainer = document.getElementById('tick-container');
-    var isFirstLoad = true; // Flag to track initial load
+document.getElementById("submit-btn").addEventListener("click", function () {
+    let tickContainer = document.getElementById('tick-container');
+    let contactForm = document.getElementById("contact-form");
 
-    iframe.onload = function () {
-        spinner.style.setProperty('display', 'none', 'important'); // Hide the spinner once the form is loaded
-
-        // If the iframe loads and it's not the first load, assume the form was submitted
-        if (!isFirstLoad) {
-            // Hide the iframe (to suppress Google's thank-you message)
-            iframe.style.display = 'none';
-
-            // Show the custom thank-you message and tick animation
-            tickContainer.style.display = 'block'; // Display the tick
-            setTimeout(function () {
-                window.scrollTo(0, 0); // Scroll to the top
-            }, 500); // Delay to simulate form submission
-        }
-
-        // Mark the first load as done
-        isFirstLoad = false;
-    };
-});
+    event.preventDefault();
+    contactForm.style.display = "none";
+    tickContainer.style.display = 'block';
+})
---
+++ js/contact2.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var iframe = document.getElementById('gForm');
+    var spinner = document.getElementById('spinner');
+    var tickContainer = document.querySelector('.tick-container');
+    var isFirstLoad = true; // Flag to track initial load
+
+    iframe.onload = function () {
+        spinner.style.setProperty('display', 'none', 'important'); // Hide the spinner once the form is loaded
+
+        // If the iframe loads and it's not the first load, assume the form was submitted
+        if (!isFirstLoad) {
+            // Hide the iframe (to suppress Google's thank-you message)
+            iframe.style.display = 'none';
+
+            // Show the custom thank-you message and tick animation
+            tickContainer.style.display = 'block'; // Display the tick
+            setTimeout(function () {
+                window.scrollTo(0, 0); // Scroll to the top
+            }, 500); // Delay to simulate form submission
+        }
+
+        // Mark the first load as done
+        isFirstLoad = false;
+    };
+});
